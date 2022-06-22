9e5c9b2e92f6457ac9c61923178cd3cc624bf4a6
afran012 <afran.012@gmail.com>
2022-06-22 02:43:18 -0500

    add API

---
+++ DB/permisos/Tablas.sql
@@ -0,0 +1,34 @@
+/************************************************************************************
+Descripci�n: Objeto de encargado de ...
+Autor:       Nombre del desarrollador y usuario
+Fecha:       01-01-2022
+IdGesti�n:   Ticket-2132123
+@copyright:  NombreDeLaEmpresa
+************************************************************************************/
+
+GRANT select, update, delete, insert ON  CARCENTERADMIN.ESTADO_MANTENIMIENTO TO CARCENTERADMIN;
+/
+GRANT select, update, delete, insert ON  CARCENTERADMIN.VEHICULO TO CARCENTERADMIN;
+/
+GRANT select, update, delete, insert ON  CARCENTERADMIN.TIPO_IDENTIFICACION TO CARCENTERADMIN;
+/
+GRANT select, update, delete, insert ON  CARCENTERADMIN.REPUESTO TO CARCENTERADMIN;
+/
+GRANT select, update, delete, insert ON  CARCENTERADMIN.REPUESTO_MANTENIMIENTO TO CARCENTERADMIN;
+/
+GRANT select, update, delete, insert ON  CARCENTERADMIN.PERSONA TO CARCENTERADMIN;
+/
+GRANT select, update, delete, insert ON  CARCENTERADMIN.MECANICO TO CARCENTERADMIN;
+/
+GRANT select, update, delete, insert ON  CARCENTERADMIN.MANTENIMIENTO TO CARCENTERADMIN;
+/
+GRANT select, update, delete, insert ON  CARCENTERADMIN.MANO_DE_OBRA TO CARCENTERADMIN;
+/
+GRANT select, update, delete, insert ON  CARCENTERADMIN.INVENTARIO TO CARCENTERADMIN;
+/
+GRANT select, update, delete, insert ON  CARCENTERADMIN.FACTURA TO CARCENTERADMIN;
+/
+GRANT select, update, delete, insert ON  CARCENTERADMIN.ESTADO_MECANICO TO CARCENTERADMIN;
+/
+
+
